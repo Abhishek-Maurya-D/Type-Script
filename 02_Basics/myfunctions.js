1663ad5819d99c82ca0dcf0a47db6e006da6cbb8
Abhishek Maurya <abhismaurya684@gmail.com>
2025-05-10 13:02:00 +0530

    during self study files are updated

--- 02_Basics/myfunctions.js
+++ 02_Basics/myfunctions.js
@@ -1,64 +1,60 @@
 "use strict";
-//  1 - function concept - number type
-// function addTwo(num: number){
-//     // num.toUpperCase()
-//     return num + 2;
-//     // return "Hello";
-// }
-// let addtwovar = addTwo(4);
-// console.log(addtwovar);
 Object.defineProperty(exports, "__esModule", { value: true });
-// 2 - string type function
-// function getUpper(val: string){
-//     return val.toUpperCase()
-// }
-// let str = getUpper("4");
-// console.log(str);
-// 3 - function inline declaration with constrants types
-// let loginUser = (name: string, email:string, isPaid: boolean = false) => {}
-// let loginUser = (name: string, email:string, isPaid) => {
-//     if (isPaid === void 0) {isPaid = false;}
-// }
-// loginUser("Abhi", "abhi@gmail.com",600)
-// console.log(loginUser);
-// 4 function declaration and its calling with parameters
-// function signUpUser(name: string, email: string, isPaid: boolean){
-//     return name;
-// }
-// let abhi = signUpUser("Abhishek", "abhishek3@gmail.com", false)
-// console.log(abhi);
-// 5 function with return type declaration -- can return both boolean or string
-// function getValue(myVal: number): boolean | string{
-//     if(myVal>6)
-//         return true;
-//     else
-//         return "200 OK";
-// }
-// console.log(getValue(4));
-// 6 function declaration with string as a type of return 
-// const getHello = (s: string):string => {
-//     return ""
-// }
-// console.log(getHello("Abhishek"));
-// 7 function declaration and changing it to map with values and printing out that
-// const heros = ["thor", "spiderman", "ironman"];
-// heros.map((hero: string) => {
-//     console.log(`hero is ${hero}`);
-// })
-// 8 function declared as a return type of string 
-// const heros = [1, 2, 3];
-// heros.map((hero) : string => {
-//     console.log(`hero is ${hero}`);
-//     return `hero is ${hero}`;
-// })
-// 9 error printing through void function
-// function consoleError (errmsg : string): void {
-//     console.log(errmsg);
-// }
-// consoleError("error");
-// 10 error printing through never
-function handleError(errmsg) {
-    console.log(errmsg);
-    throw new Error(errmsg);
+//1 - function concept - number type
+function addTwo(num) {
+    // num.toUpperCase()
+    return num + 2;
+    // return "Hello";
 }
-handleError("error");
+var addtwovar = addTwo(4);
+console.log(addtwovar);
+//2 - string type function
+function getUpper(val) {
+    return val.toUpperCase();
+}
+var str = getUpper("4");
+console.log(str);
+//3 - function inline declaration with constrants types
+//let loginUser = (name: string, email:string, isPaid: boolean = false) => {}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) {
+        isPaid = false;
+    }
+};
+loginUser("Abhi", "abhi@gmail.com", 600);
+console.log(loginUser);
+//4 function declaration and its calling with parameters
+function signUpUser(name, email, isPaid) {
+    return name;
+}
+var abhi = signUpUser("Abhishek", "abhishek3@gmail.com", false);
+console.log(abhi);
+//5 function with return type declaration -- can return both boolean or string
+function getValue(myVal) {
+    if (myVal > 6)
+        return true;
+    else
+        return "200 OK";
+}
+console.log(getValue(4));
+//6 function declaration with string as a type of return 
+var getHello = function (s) {
+    return "";
+};
+console.log(getHello("Abhishek"));
+// 7 function declaration and changing it to map with values and printing out that
+var heroes = ["thor", "spiderman", "ironman"];
+heroes.map(function (hero) {
+    console.log("hero is ".concat(hero));
+});
+// 8 function declared as a return type of string 
+var heros = [1, 2, 3];
+heros.map(function (hero) {
+    console.log("hero is ".concat(hero));
+    return "hero is ".concat(hero);
+});
+// 9 error printing through void function
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+consoleError("error");
--- 02_Basics/myfunctions.ts
+++ 02_Basics/myfunctions.ts
@@ -1,71 +1,71 @@
-//  1 - function concept - number type
-// function addTwo(num: number){
-//     // num.toUpperCase()
-//     return num + 2;
-//     // return "Hello";
-// }
-// let addtwovar = addTwo(4);
-// console.log(addtwovar);
+ //1 - function concept - number type
+function addTwo(num: number){
+    // num.toUpperCase()
+    return num + 2;
+    // return "Hello";
+}
+let addtwovar = addTwo(4);
+console.log(addtwovar);
 
-// 2 - string type function
-// function getUpper(val: string){
-//     return val.toUpperCase()
-// }
-// let str = getUpper("4");
-// console.log(str);
+//2 - string type function
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+let str = getUpper("4");
+console.log(str);
 
-// 3 - function inline declaration with constrants types
-// let loginUser = (name: string, email:string, isPaid: boolean = false) => {}
-// let loginUser = (name: string, email:string, isPaid) => {
-    //     if (isPaid === void 0) {isPaid = false;}
-// }
-// loginUser("Abhi", "abhi@gmail.com",600)
-// console.log(loginUser);
+//3 - function inline declaration with constrants types
+//let loginUser = (name: string, email:string, isPaid: boolean = false) => {}
+let loginUser = (name: string, email:string, isPaid) => {
+        if (isPaid === void 0) {isPaid = false;}
+}
+loginUser("Abhi", "abhi@gmail.com",600)
+console.log(loginUser);
 
-// 4 function declaration and its calling with parameters
-// function signUpUser(name: string, email: string, isPaid: boolean){
-//     return name;
-// }
-// let abhi = signUpUser("Abhishek", "abhishek3@gmail.com", false)
-// console.log(abhi);
+//4 function declaration and its calling with parameters
+function signUpUser(name: string, email: string, isPaid: boolean){
+    return name;
+}
+let abhi = signUpUser("Abhishek", "abhishek3@gmail.com", false)
+console.log(abhi);
 
-// 5 function with return type declaration -- can return both boolean or string
-// function getValue(myVal: number): boolean | string{
-//     if(myVal>6)
-//         return true;
-//     else
-//         return "200 OK";
-// }
-// console.log(getValue(4));
+//5 function with return type declaration -- can return both boolean or string
+function getValue(myVal: number): boolean | string{
+    if(myVal>6)
+        return true;
+    else
+        return "200 OK";
+}
+console.log(getValue(4));
 
-// 6 function declaration with string as a type of return 
-// const getHello = (s: string):string => {
-//     return ""
-// }
-// console.log(getHello("Abhishek"));
+//6 function declaration with string as a type of return 
+const getHello = (s: string):string => {
+    return ""
+}
+console.log(getHello("Abhishek"));
 
 // 7 function declaration and changing it to map with values and printing out that
-// const heros = ["thor", "spiderman", "ironman"];
-// heros.map((hero: string) => {
-//     console.log(`hero is ${hero}`);
-// })
+const heroes = ["thor", "spiderman", "ironman"];
+heroes.map((hero: string) => {
+    console.log(`hero is ${hero}`);
+})
 
 // 8 function declared as a return type of string 
-// const heros = [1, 2, 3];
-// heros.map((hero) : string => {
-//     console.log(`hero is ${hero}`);
-//     return `hero is ${hero}`;
-// })
+const heros = [1, 2, 3];
+heros.map((hero) : string => {
+    console.log(`hero is ${hero}`);
+    return `hero is ${hero}`;
+})
 
 // 9 error printing through void function
-// function consoleError (errmsg : string): void {
-//     console.log(errmsg);
-// }
-// consoleError("error");
+function consoleError (errmsg : string): void {
+    console.log(errmsg);
+}
+consoleError("error");
 
 // 10 error printing through never
 // function handleError (errmsg : string): never {
-//     throw new Error(errmsg);
+    // throw new Error(errmsg);
 // }
 // handleError("error");
 
